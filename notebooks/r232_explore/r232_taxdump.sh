0f44c75879488f0e05bde46ea666589a614bc518
Samantha Windle <sam@windle.me.uk>
2026-08-17 11:45:59 +0100

    Filtered and created the samples tables for r232 generation

---
+++ notebooks/r232_explore/r232_taxdump.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+gtdb_to_taxdump.py \
+/mnt/Projects/Sam/pipelines/Struo2/r232/bac120_taxonomy_r232.tsv \
+/mnt/Projects/Sam/pipelines/Struo2/r232/ar53_taxonomy_r232.tsv \
+> taxdump_r232.tsv
+
